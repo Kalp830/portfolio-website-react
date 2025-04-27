3fa1e72e65fa19aa1b21ea096e09bacebd60035f
kalpesh <talekar.kt@gmail.com>
2025-04-27 18:01:05 +0530

    some more code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Intro from "./components/intro/Intro";
+import Skills from "./components/skills/Skills";
+
 const Home = () => {
   return (
     <>
       <Navbar />
       <Intro />
+      <Skills/>
     </>
   );
 };
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Skills.css'
+import UiDesign from '../../assets/images/ui-design.png'
+import WebDesign from '../../assets/images/website-design.png'
+
+const Skills = () => {
+  return (
+    <>
+          <section id='skills'>
+              <h2>About Section</h2>
+     </section> 
+    </>
+  )
+}
+
+export default Skills

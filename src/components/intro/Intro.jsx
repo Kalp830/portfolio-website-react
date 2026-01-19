00a41c31464dfbb4836a1babd65cd9d2c3d98dd5
kalpesh <talekar.kt@gmail.com>
2026-01-19 22:09:10 +0530

    Added button

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -22,9 +22,10 @@ const Intro = () => {
 I'm a UI/UX and Frontend Developer and I build for the Universe.
                   </p>
                 </div>
-                <div className="downloadBtn d-flex justify-content-between m-auto col-md-4">
+                <div className="downloadBtn d-flex justify-content-between m-auto col-md-6">
                   <a href={Pdf} download className="btn btn-lg btn-outline-secondary mr-5">Download CV</a>
                   <a href="https://www.linkedin.com/in/kalpesh-talekar-05533794/" target="_blank" className="btn btn-lg btn-outline-secondary ml-3">Linkedin Profile</a>
+                  <a href="https://www.behance.net/kalpesh2283" target="_blank" className="btn btn-lg btn-outline-secondary ml-3">Behance Profile</a>
 
                 </div>
                 {/* <div className="header__profile-image-container clearfix">
